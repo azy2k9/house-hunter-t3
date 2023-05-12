3c046f04ddc7af81efc066b951c7ecfe5aaeebd9
azy2k9 <azy2k9@gmail.com>
2023-05-12 15:31:33 +0100

    Create base card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+
+interface CardProps {
+    link: string;
+    title: string;
+    features: Array<string>;
+}
+
+const Card = ({ link, title, features }: CardProps) => {
+  return (
+    <Link
+    className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+    href={link}
+  >
+    <h3 className="text-2xl font-bold">{title} →</h3>
+    <div className="text-lg pl-8">
+        {features && (
+            <ul className='list-disc list-outside'>
+                {features.map(f => {
+                    return (
+                        <li key={f}>{f}</li>
+                    )
+                })}
+            </ul>
+        )}
+    </div>
+  </Link>
+  )
+}
+
+export default Card
