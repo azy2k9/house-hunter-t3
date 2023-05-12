d4c12ad325d694755c153f004805eb7a771ba62a
azy2k9 <azy2k9@gmail.com>
2023-05-12 18:06:45 +0100

    Add a quick navbar

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 interface ContainerProps {
@@ -5,17 +6,42 @@ interface ContainerProps {
     title: string;
 }
 
+interface Navlink {
+    text: string;
+    href: string;
+}
+
+const links: Array<Navlink> = [
+    {
+        text: 'Home',
+        href: '/'
+    },
+    {
+        text: 'View Houses',
+        href: '/view-houses'
+    },
+    {
+        text: 'Add House',
+        href: '/add-house'
+    },
+]
+
 const Container = ({children, title} : ContainerProps) => {
     const { firstHalf, secondHalf, multiword } = createTitle(title);    
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-                <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] text-center">
-                    {firstHalf}{multiword && " "}<span className="text-[hsl(280,100%,70%)]">{secondHalf}</span>        
-                </h1>
-                {children}
-            </div>
-        </main>
+        <div className='bg-gradient-to-b from-[#2e026d] to-[#15162c]'>
+            <ul className='flex justify-center py-8 text-white'>
+                {links.map(link => (<Link key={link.href} href={link.href}><li className='px-3 font-bold' >{link.text}</li></Link>))}
+            </ul>
+            <main className="flex min-h-screen flex-col items-center justify-center">
+                <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+                    <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] text-center">
+                        {firstHalf}{multiword && " "}<span className="text-[hsl(280,100%,70%)]">{secondHalf}</span>        
+                    </h1>
+                    {children}
+                </div>
+            </main>
+        </div>
     )
 }
 
